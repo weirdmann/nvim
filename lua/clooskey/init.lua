05723c99e7c14e20b685844fb433a8e88ba396aa
weirdmann <hubwe@pm.me>
2024-02-25 02:05:50 +0100

    first try

--- lua/clooskey/init.lua
+++ lua/clooskey/init.lua
@@ -1,4 +1,30 @@
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+"folke/which-key.nvim",
+"catppuccin/nvim"
+})
+
 require("clooskey.remap")
 print("Hello from Clooskey config!")
 vim.cmd.colorscheme "catppuccin"
 vim.cmd.set "relativenumber" 
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    if vim.fn.argv(0) == "" then
+      require("telescope.builtin").find_files()
+    end
+  end,
+})
