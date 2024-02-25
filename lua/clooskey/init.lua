2505ac9a842ea1fdbaf0a99e3869e2908eb74b39
weirdmann <hubwe@pm.me>
2024-02-25 17:34:09 +0100

    changed my mind about the relative number

--- lua/clooskey/init.lua
+++ lua/clooskey/init.lua
@@ -1,7 +1,7 @@
 require("clooskey.remap")
 print("Hello from Clooskey config!")
 vim.cmd.colorscheme "catppuccin"
-vim.cmd.set "relativenumber" 
+vim.cmd.set "number" 
 vim.api.nvim_create_autocmd("VimEnter", {
   callback = function()
     if vim.fn.argv(0) == "" then
