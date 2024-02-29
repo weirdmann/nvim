ceb1d8220b1571f9d1f069eed519835691365628
weirdmann <hubwe@pm.me>
2024-02-29 10:37:56 +0100

    tweaks

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -15,6 +15,7 @@ enable = true,
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
     -- Using this option may slow down your editor, and you may see some duplicate highlights.
     -- Instead of true it can also be a list of languages
+    disable = {'tsv', 'csv'},
     additional_vim_regex_highlighting = false,
   },
 }
